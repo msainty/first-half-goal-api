81e8098019d2b8796f32c325acffbf8e291fa3f0
Mark <mark.sainty@gmail.com>
2018-11-17 16:39:47 +0000

    add CORS for web server

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000'])
+CORS(app, origins=['http://localhost:3000',
+                   'http://first-half-goals.s3-website-eu-west-1.amazonaws.com/'])
 
 @app.route('/')
 def handler():
