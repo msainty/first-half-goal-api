5bc826677048d5f612310159ea16b5c943497432
Mark <mark.sainty@gmail.com>
2018-11-18 10:19:28 +0000

    change CORS setting

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000',
-                   'http://first-half-goals.s3-website-eu-west-1.amazonaws.com/'])
+                   'http://first-half-goals.s3-website-eu-west-1.amazonaws.com'])
 
 @app.route('/')
 def handler():
